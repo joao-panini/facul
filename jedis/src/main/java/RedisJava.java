007770a5c1f51689d37b3e6583c76efc6191e54f
joao-panini <44535246+joao-panini@users.noreply.github.com>
2018-11-09 18:05:46 -0200

    Add files via upload

--- jedis/src/main/java/RedisJava.java
+++ jedis/src/main/java/RedisJava.java
@@ -29,7 +29,8 @@ public class RedisJava {
 		String codigopostal;
 		String listmemb;
 		int pontuacao;
-
+		double auxiliar = 0;
+		int contador = 1;
 		//Variaveis aposta
 		String rodada;
 		String partida;
@@ -47,8 +48,6 @@ public class RedisJava {
 			System.out.println("1 - LOGAR");
 			System.out.println("2 - REGISTRAR DADOS FAKE");
 			op = writer.nextInt();
-
-
 			switch(op) {
 			case 1:
 				writer = new Scanner(System.in);
@@ -77,7 +76,7 @@ public class RedisJava {
 							strConc = rodada + "_" + partida + "_" + resultadoApostado;
 							jedis.sadd("APOSTAS:"+apelido,strConc);
 							
-							String resultadoPartidaApostada = jedis.hget("rodada:"+rodada+":partida:"+partida, "resultado");
+							String resultadoPartidaApostada = jedis.hget("RODADA:"+rodada+":PARTIDA:"+partida, "RESULTADO");
 							
 							if (resultadoPartidaApostada.equals(resultadoApostado)) {
 								pontuacao = Integer.parseInt(jedis.hget("USUARIO:"+apelido, "SCORE"));
@@ -95,17 +94,23 @@ public class RedisJava {
 						}
 						break;
 					case 2:
-						writer = new Scanner(System.in);
-						System.out.println("insira o usuario que deseja listar:");
-						apelido = writer.nextLine();
-
-						Set<String> setC = jedis.smembers("APOSTAS:"+apelido);
-						for(String s : setC) {
-							String[] sCorrigida = s.split("_");
-							System.out.println("Rodada apostada:"+ sCorrigida[0]);
-							System.out.println("Partida apostada:"+ sCorrigida[1]);
-							System.out.println("Resultado apostado:"+ sCorrigida[2]);
-							System.out.println("\n");
+						continuar = "S";
+						while (continuar.equalsIgnoreCase("S")) {
+							writer = new Scanner(System.in);
+							System.out.println("\nINSIRA O USUARIO QUE DESEJA LISTAR:");
+							apelido = writer.nextLine();
+	
+							Set<String> setC = jedis.smembers("APOSTAS:"+apelido);
+							for(String s : setC) {
+								String[] sCorrigida = s.split("_");
+								System.out.println("Rodada apostada:"+ sCorrigida[0]);
+								System.out.println("Partida apostada:"+ sCorrigida[1]);
+								System.out.println("Resultado apostado:"+ sCorrigida[2]);
+								System.out.println("\n");
+							}
+							
+							System.out.println("Deseja realizar outra consulta? (S/N)");
+							continuar = writer.nextLine().toUpperCase();
 						}
 						break;
 					case 3:
@@ -114,23 +119,27 @@ public class RedisJava {
 						pontuacao = 0;
 						
 						for (String user : Usuarios) {
-							pontuacao = Integer.parseInt(jedis.hget("USUARIO:"+apelido, "SCORE"));
+							pontuacao = Integer.parseInt(jedis.hget("USUARIO:"+user, "SCORE"));
 							jedis.zadd("RANKING",pontuacao,user);
 						}
 						
-						Ranking = jedis.zrangeWithScores("RANKING", 0, -1);
+						Ranking = jedis.zrevrangeWithScores("RANKING", 0, -1);
 						System.out.println("--- RANKING ---\n");
 						for (Tuple rtuple : Ranking) {
-							System.out.println("Apelido: "+ rtuple.getElement());
-							System.out.println("Pontos: "+ rtuple.getScore()+ "\n");
+							if(rtuple.getScore() != auxiliar) {
+								System.out.println("Posição: " + contador);
+								System.out.println("Pontos: " + rtuple.getScore());
+								System.out.println("Apelido: "+ rtuple.getElement() + "\n");
+								contador++;
+							}
 						}
 						
-						
-						
 					}
+					break;
 
 
 				}else {
+					System.out.println("USUARIO NÃO CADASTRADO! \n \n");
 					System.out.println("---CADASTRO USUARIO---");
 
 					System.out.println("Insira seu apelido:");
@@ -174,6 +183,8 @@ public class RedisJava {
 					jedis.hset("USUARIO:"+apelido,"CODIGO_POSTAL", codigopostal);
 					
 					jedis.hset("USUARIO:"+apelido, "SCORE" , "0");
+					
+					System.out.println("USUARIO CADASTRADO COM SUCESSO!!");
 					break;
 				}
 
@@ -184,55 +195,78 @@ public class RedisJava {
 				int[] rodadas;
 				char c;
 				rodadas = new int[38];
-				for (int i = 1; i <= 38; i++) {
-					for (int j = 1; j <= 10; j++) {
-						Random r = new Random();
-						c = alf.charAt(r.nextInt(N));
-						String s = Character.toString(c);
-						jedis.hset("rodada:"+i+":partida:"+j, "resultado", s);
-						String paraprintar = jedis.hget("rodada:"+i+":partida:"+j, "resultado");
-						System.out.println("Rodada " + i + " Partida " + j + " Resultado " + paraprintar);
-					}
-				}
+				
 				jedis.zadd("APELIDO",1,"JOAO");
 				jedis.zadd("APELIDO",1,"PROFESSOR");
 				jedis.zadd("APELIDO",1,"ALUNO");
 				jedis.zadd("APELIDO",1,"TESTE");
 				
+				jedis.hset("USUARIO:JOAO","APELIDO", "JOAO");
+				jedis.hset("USUARIO:PROFESSOR","APELIDO", "PROFESSOR");
+				jedis.hset("USUARIO:ALUNO","APELIDO", "ALUNO");
+				jedis.hset("USUARIO:TESTE","APELIDO", "TESTE");
 				
-				jedis.sadd("APOSTAS:JOAO","1_1_B");
-				jedis.sadd("APOSTAS:JOAO","2_1_A");
-				jedis.sadd("APOSTAS:JOAO","3_1_B");
-				jedis.sadd("APOSTAS:JOAO","4_1_E");
-				jedis.sadd("APOSTAS:JOAO","5_1_B");
-				jedis.sadd("APOSTAS:JOAO","6_1_A");
-				jedis.sadd("APOSTAS:JOAO","7_1_B");
-				jedis.sadd("APOSTAS:JOAO","8_1_E");
-				jedis.sadd("APOSTAS:JOAO","9_1_B");
-				jedis.sadd("APOSTAS:JOAO","10_1_A");
+				jedis.hset("USUARIO:JOAO", "SCORE" , "0");
+				jedis.hset("USUARIO:PROFESSOR", "SCORE" , "0");
+				jedis.hset("USUARIO:ALUNO", "SCORE" , "0");
+				jedis.hset("USUARIO:TESTE", "SCORE" , "0");
 				
-				jedis.sadd("APOSTAS:PROFESSOR","1_2_B");
-				jedis.sadd("APOSTAS:PROFESSOR","2_2_A");
-				jedis.sadd("APOSTAS:PROFESSOR","3_2_B");
-				jedis.sadd("APOSTAS:PROFESSOR","4_2_E");
-				jedis.sadd("APOSTAS:PROFESSOR","5_2_B");
-				jedis.sadd("APOSTAS:PROFESSOR","6_2_A");
-				jedis.sadd("APOSTAS:PROFESSOR","7_2_B");
-				jedis.sadd("APOSTAS:PROFESSOR","8_2_E");
-				jedis.sadd("APOSTAS:PROFESSOR","9_2_B");
-				jedis.sadd("APOSTAS:PROFESSOR","10_2_A");
-				
-				jedis.sadd("APOSTAS:ALUNO","");
-				
-				
-				
-				
+				for (int i = 1; i <= 38; i++) {
+					for (int j = 1; j <= 10; j++) {
+						//RESULTADOS FAKE DAS PARTIADS
+						Random r = new Random();
+						c = alf.charAt(r.nextInt(N));
+						String s = Character.toString(c);
+						jedis.hset("RODADA:"+i+":PARTIDA:"+j, "RESULTADO", s);
+						String result = jedis.hget("RODADA:"+i+":PARTIDA:"+j, "RESULTADO");
+						System.out.println("Rodada " + i + " Partida " + j + " Resultado " + result);
+						
+						//APOSTAS FAKE COM VALIDACAO E SCORE
+						c = alf.charAt(r.nextInt(N));
+						s = Character.toString(c);
+						jedis.sadd("APOSTAS:JOAO", i + "_" + j + "_" + s);						
+						if (result.equals(s)) {
+							pontuacao = 0;
+							pontuacao = Integer.parseInt(jedis.hget("USUARIO:JOAO", "SCORE"));
+							pontuacao++;
+							jedis.hset("USUARIO:JOAO", "SCORE", Integer.toString(pontuacao));
+						}
+						
+						c = alf.charAt(r.nextInt(N));
+						s = Character.toString(c);
+						jedis.sadd("APOSTAS:PROFESSOR", i + "_" + j + "_" + s);						
+						if (result.equals(s)) {
+							pontuacao = 0;
+							pontuacao = Integer.parseInt(jedis.hget("USUARIO:PROFESSOR", "SCORE"));
+							pontuacao++;
+							jedis.hset("USUARIO:PROFESSOR", "SCORE", Integer.toString(pontuacao));
+						}
+						
+						c = alf.charAt(r.nextInt(N));
+						s = Character.toString(c);
+						jedis.sadd("APOSTAS:ALUNO", i + "_" + j + "_" + s);						
+						if (result.equals(s)) {
+							pontuacao = 0;
+							pontuacao = Integer.parseInt(jedis.hget("USUARIO:ALUNO", "SCORE"));
+							pontuacao++;
+							jedis.hset("USUARIO:ALUNO", "SCORE", Integer.toString(pontuacao));
+						}
+						
+						c = alf.charAt(r.nextInt(N));
+						s = Character.toString(c);
+						jedis.sadd("APOSTAS:TESTE", i + "_" + j + "_" + s);						
+						if (result.equals(s)) {
+							pontuacao = 0;
+							pontuacao = Integer.parseInt(jedis.hget("USUARIO:TESTE", "SCORE"));
+							pontuacao++;
+							jedis.hset("USUARIO:TESTE", "SCORE", Integer.toString(pontuacao));
+						}
+						
+					}
+				}
+
+
 				break;
-			case 3:
-				jedis.hset("rodada:1:partida:1", "resultado", "a");
-				resultadofoda =jedis.hget("rodada:1:partida:1", "resultado");
-				break;
-				
 				default:
 					System.out.println("opcao invalida");
 			}
